55c8d1a6903589d6443b444dd08d4b515f1fa1f7
Eric Muir <eric.muir@xdesign.com>
2022-05-25 17:17:12 +0100

    Add umami tracking code

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'tailwindcss/tailwind.css'
 import Head from "next/head";
+import Script from "next/script";
 
 function App({ Component, pageProps }) {
   return (
@@ -9,6 +10,11 @@ function App({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet" />
       </Head>
+      <Script
+        async
+        defer
+        data-website-id="9102799d-8243-4419-8e77-eaec872feaf8"
+        src="https://analytics.ebdm.dev/umami.js" />
       <Component {...pageProps} />
     </>
   )
