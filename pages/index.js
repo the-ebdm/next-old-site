d7790eb9f52cbacf505fcd72d4f067a199f4542b
Eric Muir <eric.muir@xdesign.com>
2022-07-04 22:27:02 +0200

    :sparkles: Local svg icons and client id

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Tippy from '@tippyjs/react';
 import { getClient } from '../lib/ld'
+import { v4 as uuidv4 } from 'uuid'
+import Cookies from 'cookies'
 
 function SocialIcons({ icons }) {
   return <div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
@@ -24,13 +26,13 @@ const icons = [
     name: 'GitHub',
     tooltip: 'ebdmuir',
     url: 'https://github.com/ebdmuir',
-    icon: () => <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>GitHub</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" /></svg>
+    icon: '/social/github.svg'
   },
   {
     name: 'Instagram',
     tooltip: 'EricBDMuir',
     url: 'https://www.instagram.com/ericbdmuir/',
-    icon: () => <svg className="h-6 fill-current text-gray-600 hover:text-green-700" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" /></svg>
+    icon: '/social/instagram.svg'
   }
 ]
 
@@ -104,12 +106,18 @@ function Home({ flags }) {
   );
 }
 
-export async function getServerSideProps({ req }) {
-  console.log(req.cookies)
+export async function getServerSideProps({ req, res }) {
+  const cookies = new Cookies(req, res)
   const client = await getClient();
 
+  if (cookies.get('client-id')) {
+  } else {
+    const id = uuidv4();
+    cookies.set('client-id', id)
+  }
+
   let flags = await client.allFlagsState({
-    'key': req.cookies.ajs_anonymous_id,
+    'key': cookies.get('client-id'),
     'anonymous': true
   })
 
