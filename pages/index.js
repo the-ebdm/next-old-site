11addb539f357880d413fc77650c89c0b66ae3e2
Eric Muir <eric.muir@xdesign.com>
2022-08-28 14:13:55 +0100

    :fire: Stripped out LaunchDarkly due to cost :'(

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Tippy from '@tippyjs/react';
-import { getClient } from '../lib/ld'
-import { v4 as uuidv4 } from 'uuid'
-import Cookies from 'cookies'
+import personalData from '../me.json';
 
 function SocialIcons({ icons }) {
   return <div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
     {icons.map(({ url, icon: Icon, tooltip, name }) => {
-      return <Tippy content={`${name}: ${tooltip}`}>
+      return <Tippy content={`${name}: ${tooltip}`} key={name}>
         <a href={url} className={`umami--click--${name.toLowerCase()}`}>
           {typeof Icon === 'function' ? <Icon /> : <img className="h-6 fill-current text-gray-600 hover:text-green-700" src={Icon}/>}
         </a>
@@ -15,30 +13,7 @@ function SocialIcons({ icons }) {
   </div>
 }
 
-const icons = [
-  {
-    name: 'Twitter',
-    tooltip: '@EricBDMuir',
-    url: 'https://twitter.com/EricBDMuir',
-    icon: '/social/twitter.svg'
-  },
-  {
-    name: 'GitHub',
-    tooltip: 'ebdmuir',
-    url: 'https://github.com/ebdmuir',
-    icon: '/social/github.svg'
-  },
-  {
-    name: 'Instagram',
-    tooltip: 'EricBDMuir',
-    url: 'https://www.instagram.com/ericbdmuir/',
-    icon: '/social/instagram.svg'
-  }
-]
-
-function Home({ flags }) {
-  const mailingList = false;
-  console.log(flags)
+function Home({}) {
   return (
     <div className="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto my-32 lg:my-0">
 
@@ -54,19 +29,19 @@ function Home({ flags }) {
           <div className="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
           <p className="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">
             <svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" /></svg>
-            <Tippy content="Basically, I'm a massive fucking nerd"><span>{flags['job-title']}</span></Tippy>
+            <Tippy content="Basically, I'm a massive fucking nerd"><span>{personalData['job-title']}</span></Tippy>
           </p>
           <p className="pt-2 text-gray-600 text-xs lg:text-sm flex items-center justify-center lg:justify-start">
             <svg className="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M10 20a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm7.75-8a8.01 8.01 0 0 0 0-4h-3.82a28.81 28.81 0 0 1 0 4h3.82zm-.82 2h-3.22a14.44 14.44 0 0 1-.95 3.51A8.03 8.03 0 0 0 16.93 14zm-8.85-2h3.84a24.61 24.61 0 0 0 0-4H8.08a24.61 24.61 0 0 0 0 4zm.25 2c.41 2.4 1.13 4 1.67 4s1.26-1.6 1.67-4H8.33zm-6.08-2h3.82a28.81 28.81 0 0 1 0-4H2.25a8.01 8.01 0 0 0 0 4zm.82 2a8.03 8.03 0 0 0 4.17 3.51c-.42-.96-.74-2.16-.95-3.51H3.07zm13.86-8a8.03 8.03 0 0 0-4.17-3.51c.42.96.74 2.16.95 3.51h3.22zm-8.6 0h3.34c-.41-2.4-1.13-4-1.67-4S8.74 3.6 8.33 6zM3.07 6h3.22c.2-1.35.53-2.55.95-3.51A8.03 8.03 0 0 0 3.07 6z" /></svg>
-            <Tippy content="Middle of fuckin' nowhere mate"><span>{flags['location']}</span></Tippy>
+            <Tippy content="Middle of fuckin' nowhere mate"><span>{personalData['location']}</span></Tippy>
           </p>
           <p className="pt-8 text-sm">
-            {flags['intro']}
+            {personalData['intro']}
           </p>
           {/* <p className="py-2 text-sm">
             Hit me up on one of the platforms below if you want to collaborate or even just natter!
           </p> */}
-          {flags['mailing-list'] ?
+          {personalData['mailing-list'] ?
             <div className="pt-12 pb-8">
               <button className="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
                 Get In Touch
@@ -74,7 +49,7 @@ function Home({ flags }) {
             </div>
             : null}
 
-          <SocialIcons icons={icons} />
+          <SocialIcons icons={personalData['social-icons']} />
 
         </div>
 
@@ -98,40 +73,12 @@ function Home({ flags }) {
 
 
       {/* <!-- Pin to top right corner --> */}
-      {/* <div className="absolute top-0 right-0 h-12 w-18 p-4">
+      <div className="absolute top-0 right-0 h-12 w-18 p-4">
         <button className="js-change-theme focus:outline-none">🌙</button>
-      </div> */}
+      </div>
 
     </div>
   );
-}
-
-export async function getServerSideProps({ req, res }) {
-  const cookies = new Cookies(req, res)
-  const client = await getClient();
-
-  let id = cookies.get('client-id')
-
-  if (id) {
-    console.log(`Welcome back ${id}`)
-  } else {
-    id = uuidv4();
-    cookies.set('client-id', id)
-  }
-
-  let flags = await client.allFlagsState({
-    'key': id,
-    'anonymous': true
-  })
-
-  console.log(flags.toJSON())
-
-  return {
-    props: {
-      flags: flags.toJSON(),
-      mailingList: true,
-    },
-  };
 }
 
 export default Home;
