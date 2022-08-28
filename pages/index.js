dce771b12d2beed1dc8a6383d26688cf6075122a
Eric Muir <eric.muir@xdesign.com>
2022-08-28 19:07:01 +0100

    🐛 Fix Social Icon tooltips

--- pages/index.js
+++ pages/index.js
@@ -6,11 +6,13 @@ function SocialIcons({ icons }) {
   return <div className="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
     {icons.map(({ url, icon: Icon, tooltip, name }) => {
       return <Tippy content={`${name}: ${tooltip}`} key={name}>
-        <Link href={url} passHref prefetch>
-          <a className={`umami--click--${name.toLowerCase()}`}>
-            {typeof Icon === 'function' ? <Icon /> : <img className="h-6 fill-current text-gray-600 hover:text-green-700" src={Icon}/>}
-          </a>
-        </Link>
+        <div>
+          <Link href={url} passHref prefetch>
+            <a className={`umami--click--${name.toLowerCase()}`}>
+              {typeof Icon === 'function' ? <Icon /> : <img className="h-6 fill-current text-gray-600 hover:text-green-700" src={Icon}/>}
+            </a>
+          </Link>
+        </div>
       </Tippy>
     })}
   </div>
