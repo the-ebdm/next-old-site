8c6efdee72f324eb7c9b0e018b443cf5e5a035fe
Eric Muir <eric.muir@xdesign.com>
2022-09-01 10:24:20 +0100

    ⚰️ Remove dead code

--- pages/blog/[post].js
+++ pages/blog/[post].js
@@ -1,17 +1,15 @@
 import Post from "../../components/Molecule/post";
-import axios from "axios";
 import { NotionAPI } from "notion-client";
-import { useRouter } from "next/router";
 import { Client } from "@notionhq/client"
-import { recordMapParser } from "../../lib/notion";
 import BreadCrumbNav from "../../components/Atom/breadcrumbNav";
+
 const notion = new Client({
     auth: process.env.NOTION_TOKEN,
 })
+
 const dbid = "3a58bc30-8715-46d4-814f-ed9f777b2a72"
 
 export async function getStaticPaths() {
-    const client = new NotionAPI();
     const { results } = await notion.databases.query({
         database_id: dbid,
         filter: {
@@ -22,9 +20,7 @@ export async function getStaticPaths() {
         }
     });
 
-    const ids = results.map(item => ({"params": {"post": item.id}}))
-
-    console.log(ids)
+    const ids = results.map(item => ({ "params": { "post": item.id } }))
 
     return {
         paths: ids,
@@ -33,19 +29,15 @@ export async function getStaticPaths() {
 }
 
 export const getStaticProps = async (context) => {
-    console.log(context)
     const client = new NotionAPI();
     const recordMap = await client.getPage(context.params.post);
     const page = await notion.pages.retrieve({ page_id: context.params.post });
 
     const blocks = await notion.blocks.children.list({ block_id: context.params.post }).then(blocks => {
-        const { results } = blocks;
         return blocks;
     })
 
     page.blocks = blocks;
-
-    console.log(page)
 
     return {
         props: {
