2761029daa3056b85f4bcf9e24780d0996c8b9a0
Eric Muir <eric.muir@xdesign.com>
2022-09-01 10:22:43 +0100

    ✨ Refactor and add comments

--- components/Molecule/post.js
+++ components/Molecule/post.js
@@ -1,139 +1,46 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { recordMapParser } from "../../lib/notion";
+import Panel from "../Atom/panel";
 import RenderNotionBlock from "../Atom/renderBlock";
+import { useScript } from "../../lib/hooks";
 
 export default function Post({ post, recordMap }) {
-  const [language, setLanguage] = useState("javascript");
-  const [showLineNumber, setShowLineNumber] = useState(true);
+  const comment = useRef(null);
+  const status = useScript({
+    url: "https://utteranc.es/client.js",
+    theme: "github-light",
+    issueTerm: "pathname",
+    repo: "ebdmuir/website",
+    ref: comment
+  });
+
   if (post) {
     return (
-      <div className="relative py-16 bg-white overflow-hidden' mx-auto">
-        <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
-          <div
-            className="relative h-full text-lg max-w-prose mx-auto"
-            aria-hidden="true"
-          >
-            <svg
-              className="absolute top-12 left-full transform translate-x-32"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-            >
-              <defs>
-                <pattern
-                  id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={384}
-                fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
-              />
-            </svg>
-            <svg
-              className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-            >
-              <defs>
-                <pattern
-                  id="f210dbf6-a58d-4871-961e-36d5016a0f49"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={384}
-                fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
-              />
-            </svg>
-            <svg
-              className="absolute bottom-12 left-full transform translate-x-32"
-              width={404}
-              height={384}
-              fill="none"
-              viewBox="0 0 404 384"
-            >
-              <defs>
-                <pattern
-                  id="d3eb07ae-5182-43e6-857d-35c643af9034"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect
-                    x={0}
-                    y={0}
-                    width={4}
-                    height={4}
-                    className="text-gray-200"
-                    fill="currentColor"
-                  />
-                </pattern>
-              </defs>
-              <rect
-                width={404}
-                height={384}
-                fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
-              />
-            </svg>
-          </div>
+      <Panel padding="top" addClass="px-8">
+        <div className="text-lg max-w-prose mx-auto">
+          <h1>
+            <span className="block text-base text-center text-teal-600 font-semibold tracking-wide uppercase">
+              Introducing
+            </span>
+            <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              {post.properties.Name.title[0].plain_text}
+            </span>
+          </h1>
+          {post.properties.hasOwnProperty('Description') ? (
+            <p className="mt-8 text-xl text-gray-500 leading-8 text-center">
+              {post.properties.Description.rich_text[0].plain_text}
+            </p>
+          ) : null}
         </div>
-        <div className="relative px-4 sm:px-6 lg:px-8 w-3/4 mx-auto">
-          <div className="text-lg max-w-prose mx-auto">
-            <h1>
-              <span className="block text-base text-center text-teal-600 font-semibold tracking-wide uppercase">
-                Introducing
-              </span>
-              <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                {post.properties.Name.title[0].plain_text}
-              </span>
-            </h1>
-            { post.properties.hasOwnProperty('Description') ? (
-              <p className="mt-8 text-xl text-gray-500 leading-8 text-center">
-                {post.properties.Description.rich_text[0].plain_text}
-              </p>
-            ) : null}
-          </div>
-          <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-            {recordMapParser(recordMap).map((block) => (
-              <RenderNotionBlock block={block} key={block.id}/>
-            ))}
-          </div>
+        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          {recordMapParser(recordMap).map((block) => (
+            <RenderNotionBlock block={block} key={block.id} />
+          ))}
+          {
+            <div ref={comment}></div>
+          }
         </div>
-      </div>
+      </Panel>
     );
   } else {
     return <div>Loading...</div>;
